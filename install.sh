e44a8cc3dc88cf5585de0c472431d575b0fd0ade
Gautam Thakur <117568564+gautamthakur08@users.noreply.github.com>
2023-03-17 08:26:35 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+# an mpv player for playing the video
+# yt_dlp module for download youtube video
+# PySimpleGUI for the frontend of the app
+
+# checking for root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# install requirements
+apt-get install yt_dlp mpv_player
+python3 -m pip install PySimpleGUI
+
+chmod +x ./utube.py
+mv ./utube.py /usr/bin/utube
